187be75e182297674b31c4764014e3c1a4da61e4
EL-IDRISSI-Imad <iei.2002.imad@gmail.com>
2024-01-03 12:57:32 -0800

    update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-string = "last digit of"
-inttostr = str(number)
-print(inttostr)
-n = int(inttostr[-1])
-if number > 0:
-    if int(inttostr[-1]) > 5:
-        print (string + f"{number:d}" + "is" + f"{n:d}" + "and is greater than 5")
-    elif int(inttostr[-1]) == 0:
-        print (string + f"{number:d}" + "is" + f"{n:d}" + "and is 0")
-    else:
-        print (string + f"{number:d}" + "is " + f"{n:d}" + "and is less than 6 and not 0")
-elif number == 0:
-    print(string + f"{number:d}" + "is" + f"{n:d}" + "and is 0")
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+print(f"Last digit of {number:d} is {digit:d} and is ", end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
 else:
-    if int(inttostr[-1]) > 5 or int(inttostr[-1]) < 5:
-        print (string + f"{number:d}" + "is" + f"-{n:d}" + "and is less than 6 and not 0")
-    else:
-        print (string + f"{number:d}" + "is" + f"{n:d}" + "and is 0")
+    print("less than 6 and not 0")
