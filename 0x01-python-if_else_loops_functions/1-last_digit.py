721877bc02b6dd3c83c41131c39b47ff7cb8d750
EL-IDRISSI-Imad <iei.2002.imad@gmail.com>
2024-01-03 12:32:04 -0800

    update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,22 @@ import random
 number = random.randint(-10000, 10000)
 
 string = "last digit of "
-inttosrt = str(number)
-n = int(inttosrt[-1])
-
-if int(inttosrt[-1]) > 5:
-    print (string + f"{number:d}" + " " + "is " + f"{n:d} " + "and is greater than 5")
-elif int(inttosrt[-1]) == 0 :
-    print (string + f"{number:d}" + " " + "is " + f"{n:d} " + "and is 0")
+inttostr = str(number)
+print(inttostr)
+n = int(inttostr[-1])
+if number > 0:
+    if int(inttostr[-1]) > 5:
+        print (string + f"{number:d}" + " " + "is " + f"{n:d} " + "and is greater than 5")
+    elif int(inttostr[-1]) == 0:
+        print (string + f"{number:d}" + " " + "is " + f"{n:d} " + "and is 0")
+    else:
+        print (string + f"{number:d}" + " " + "is " + f"{n:d} " + "and is less than 6 and not 0")
+elif number == 0:
+    print(string + f"{number:d}" + " " + "is " + f"{n:d} " + "and is 0")
 else:
-    print (string + f"{number:d}" + " " + "is " + f"{n:d} " + "and is less than 6 and not 0")
+    if int(inttostr[-1]) > 5:
+        print (string + f"{number:d}" + " " + "is " + f"-{n:d} " + "and is greater than 5")
+    elif int(inttostr[-1]) == 0:
+        print (string + f"{number:d}" + " " + "is " + f"{n:d} " + "and is 0")
+    else:
+        print (string + f"{number:d}" + " " + "is " + f"-{n:d} " + "and is less than 6 and not 0")
